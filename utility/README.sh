3e9cfe8d7af28f7933b36b56ecfd3a9d0358d5c8
KING JAMES HMRH <Willtech.obdc>
2026-04-24 14:35:52 +1000

    utility/README.sh Update.
    Signed-off-by: KING JAMES HMRH <Willtech.obdc>

--- utility/README.sh
+++ utility/README.sh
@@ -1,4 +1,5 @@
 #!/bin/ksh
+echo "After successful \`make && make test && doas make install && doas makewhatis\`"
 echo "1. The send folder already has a sample random.bin"
 echo "2. run \`./send.sh\` prepares for transfer."
 echo "3. run \`./transfer.sh\` simulates transfer."
