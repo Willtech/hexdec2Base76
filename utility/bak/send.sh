90cd5cef256bdaef71e1398b3ae8b291e246ccc1
Willtech <willtech@live.com.au>
2026-04-24 17:06:28 +1000

    utility/bak/send.sh Initial Commit.

---
+++ utility/bak/send.sh
@@ -0,0 +1,10 @@
+#!/bin/ksh
+# Exit immediately if any command returns a non-zero status
+set -e
+
+cd send
+
+split -b 100k random.bin chunk_ && \
+find . -name "chunk_*" | xargs -P 8 -I {} sh -c 'pv -N "{}" "{}" | b76pipe_stream -o "{}.b76" && rm "{}"' && \
+sha256 !(*.SUM) > SHA256.SUM && sha256 -c SHA256.SUM && \
+echo "Check Okay!" || echo "Check Failed!"
